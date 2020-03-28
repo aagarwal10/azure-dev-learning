2343b31cbd51f027302477e4269d9a2253b2a797
Abhishek Agarwal <agarwal.1983@gmail.com>
2020-03-27 19:29:33 -0500

    used CICD for App Service via Webhook

--- Containers/Create-Image/SampleWebApp/build-image.ps1
+++ Containers/Create-Image/SampleWebApp/build-image.ps1
@@ -1,8 +1,11 @@
-# build our docker image and tag it v2
-docker build -t samplewebapp:v2 .
+# before we start, we should have built the sample web app with
+dotnet publish -c Release -o out
+
+# build our docker image and tag it
+docker build -t samplewebapp:v4 .
 
 # run our image
-docker run -d -p 8080:80 samplewebapp:v2 --name myappv2
+docker run -d -p 8080:80 samplewebapp:v4 --name myappv4
 
 # test it
 http://localhost:8080
