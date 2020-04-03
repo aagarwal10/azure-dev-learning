2b1720cc92bae7f463f0b39222af29f097ca0dd3
Abhishek Agarwal <agarwal.1983@gmail.com>
2020-04-02 21:28:57 -0500

    VM connect to Blob storage using managed identity

---
+++ Managed-Identites/Virtual-Machine-Blob/Storage-Account-System-MI.ps1
@@ -0,0 +1,17 @@
+﻿# This uses the system-generated Managed Identity to connect
+Connect-AzAccount -MSI 
+
+#Create a Storage Account Context
+$context = New-AzStorageContext -StorageAccountName testrgmi -UseConnectedAccount
+
+#Create a Container within
+New-AzStorageContainer -Name docs -Context $context
+
+#Create sample text file
+echo "Hello World!!" > hello.txt
+
+cat .\hello.txt
+
+#Upload the file to Container 
+Set-AzStorageBlobContent -File .\hello.txt -Container docs -Blob helloworld.txt -BlobType Block -Context $context -Verbose
+
---
+++ Managed-Identites/Virtual-Machine-Blob/Storage-Account-UserAssigned-MI.ps1
@@ -0,0 +1,16 @@
+﻿# This uses the system-generated Managed Identity to connect
+Connect-AzAccount -Identity
+
+#Create a Storage Account Context
+$context = New-AzStorageContext -StorageAccountName testrgmi -UseConnectedAccount -Verbose
+
+#Create a Container within (fails since User Assigned Managed Identity is only at Container level and not Storage Account level)
+New-AzStorageContainer -Name docstest -Context $context
+
+#Create sample text file
+echo "Hello World2!!" > hello2.txt
+
+cat .\hello2.txt
+
+#Upload the file to Container 
+Set-AzStorageBlobContent -File .\hello2.txt -Container files -Blob helloworld2.txt -BlobType Block -Context $context -Verbose
